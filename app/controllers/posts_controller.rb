27bb453e06aaa942d74a5e86fe169cea565eb906
Jamunabai Prakash <saigalprakash@gmail.com>
2018-04-18 14:00:49 -0400

    did post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,52 @@
 class PostsController < ApplicationController
+
+    before_action :find_post, only: [:show, :edit, :update, :delete]
+
+    def index
+        @posts = Post.all
+    end
+
+    def show
+    end
+
+    def new
+        @posts = Post.new
+    end
+
+    def create
+      @post = Post.new(post_params)
+      if @post.save
+          redirect_to @post, notice: "the post was created"
+      else
+          render 'new'
+      end
+
+    end
+
+    def edit
+    end
+
+    def update
+      if @post.update(post_params)
+          redirect_to @post, notice: "update is successful"
+      else
+          render 'edit'
+      end
+    end
+
+    def delete
+      @post.destroy
+      redirect_to root_path, notice: "post destroyed"
+    end
+
+private
+
+    def post_params
+      params.require(:post).permit(:title, :content, :user_id)
+    end
+
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
 end
