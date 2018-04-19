a8742cb73e29e67daaf51da72e0bcb427ed68059
Jamunabai Prakash <saigalprakash@gmail.com>
2018-04-19 08:02:30 -0400

    added to the seed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users do
+
       resources :posts
-  end
-
-  get '/feed', to: 'posts#feed'
 
 
-
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
-
-  root 'users#index'
+  root 'posts#index'
 
 end
